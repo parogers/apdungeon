7dcc9b1403ec3dd83ec4c0bb060f8544a0a8a1b0
Peter Rogers <peter.rogers@gmail.com>
2023-04-22 13:25:44 -0400

    chore: add game files into new angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PlayPageComponent } from './pages/play-page/play-page.component';
+
+const routes: Routes = [
+    {
+        path: 'play',
+        component: PlayPageComponent,
+    },
+    {
+        path: '',
+        component: PlayPageComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayPageComponent } from './pages/play-page/play-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayPageComponent
   ],
   imports: [
     BrowserModule,
